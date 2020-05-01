6e21e3e97c663fe76d0585f5b67d07b327b13f2d
Your Name <vimalpatel@vimals-mbp.home>
2020-04-30 20:15:45 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+ $(document).ready(function() { 
+
+ $("#go").click(function() {
+   
+ $("#recommendationOne").removeClass("is-hidden")
+ $("#recommendationTwo").removeClass("is-hidden")
+ $("#recommendationThree").removeClass("is-hidden")
+ $("#recommendationFour").removeClass("is-hidden")
+ $("#recommendationFive").removeClass("is-hidden")
+});
+
+
+});
