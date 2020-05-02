c3aeca1c1011713c6a60489f67aaf7057c96ff2b
MD24 <matiasdaldi@gmail.com>
2020-05-02 11:26:09 -0400

    fixed some stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "hotels4.p.rapidapi.com",
+        "x-rapidapi-key": "21280359f4mshe424cc8187c3cf9p18ebe4jsnd3a2e2520bc5"
+    }};
 var searchResults
 var hotels = [];
 var hotelNames = []
@@ -11,54 +20,18 @@ $("#go").click(function() {
     $("#recommendationFive").removeClass("is-hidden")
 });
 
-
-
-
 $(".button").on("click", function() {
     var searchTerm = $(".input").val()
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + searchTerm,
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "hotels4.p.rapidapi.com",
-        "x-rapidapi-key": "21280359f4mshe424cc8187c3cf9p18ebe4jsnd3a2e2520bc5"
-    }
-}
-$.ajax(settings).done(function (response) {
-    console.log(response);
-    var desID = response.suggestions[0].entities[0].destinationId
-    var secondSettings = {
-
-        "async": true,
-        "crossDomain": true,
-        "url": "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + searchTerm,
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "hotels4.p.rapidapi.com",
-            "x-rapidapi-key": "21280359f4mshe424cc8187c3cf9p18ebe4jsnd3a2e2520bc5"
-        }
-    }
+    settings.url = "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + searchTerm
     $.ajax(settings).then(function (response) {
         console.log(response);
         var desID = response.suggestions[0].entities[0].destinationId
-        var secondSettings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://hotels4.p.rapidapi.com/properties/list?currency=USD&locale=en_US&sortOrder=PRICE&destinationId=" + desID + "&pageNumber=1&pageSize=5",
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "hotels4.p.rapidapi.com",
-                "x-rapidapi-key": "21280359f4mshe424cc8187c3cf9p18ebe4jsnd3a2e2520bc5"
-            }
-        }
-        $.ajax(secondSettings).then(function (secondResponse) {
+        settings.url = "https://hotels4.p.rapidapi.com/properties/list?currency=USD&locale=en_US&sortOrder=PRICE&destinationId=" + desID + "&pageNumber=1&pageSize=5"
+        $.ajax(settings).then(function (secondResponse) {
             console.log(secondResponse);
             searchResults = secondResponse.data.body.searchResults
             getHotelNames();
             var hotels = searchResults.results
-            console.log(searchResults);
             console.log(hotels)
             console.log(hotelNames)
         });
@@ -67,9 +40,9 @@ $.ajax(settings).done(function (response) {
 
 function saveSearches() {
     localStorage.setItem("Searches", JSON.stringify(searches))
-}
+};
 
 function getHotelNames(){
     for (var i = 0; i < 5; i++){
         hotelNames.push(searchResults.results[i].name)
-}
+}};
